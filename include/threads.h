67f443f9d6f56d8db16bdc1f541e8d6a032a9e5f
arakich <j3shw4@gmail.com>
2020-09-05 14:10:05 -0400

    share intake control var

--- include/threads.h
+++ include/threads.h
@@ -1,5 +1,9 @@
 #include "main.h"
 using namespace okapi;
+extern int intakeState;
+extern int inward;
+extern int outward;
+extern int stop;
 extern void thread_conveyor(void*p);
 extern void thread_intake(void*p);
 extern void thread_control(void*p);
