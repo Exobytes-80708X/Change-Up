311389899b03f20bee746ad0d715bd0e3b47fcbc
RushilSidhu <49910626+RushilSidhu@users.noreply.github.com>
2020-11-25 15:12:27 -0500

    ss

--- src/PurePursuit.cpp
+++ src/PurePursuit.cpp
@@ -5,13 +5,13 @@
 typedef double db;
 typedef std::vector<int> vi;
 
-vi circLineIntersect(db xl, db yl, db a, db b, db r, db xc, db yc){
+db circLineIntersect(db xl, db yl, db a, db b, db r, db xc, db yc){
   vi ret(2);
   db x0 = xl-xc;
   db y0 = yl-yc;
   db radical = pow((a*x0 + b*y0),2.0) - (a*a + b*b)*(x0*x0 + y0*y0 - r*r);
   if(radical < 0)
-    return ret;
+    return -1;
 
   db negB = -(a*x0 + b*y0);
   db denom = a*a + b*b;
@@ -22,7 +22,10 @@ vi circLineIntersect(db xl, db yl, db a, db b, db r, db xc, db yc){
       ret.push_back(ret1);
   if(ret2 >= 0.0 && ret2 <= 1.0)
       ret.push_back(ret2);
-  return ret;
+
+  if(ret.size() == 1)
+    return ret[1];
+  return fmax(ret[1],ret[2]);
 }
 
 void findFurthestPoint(vi xPts, vi yPts, db r)
@@ -41,4 +44,6 @@ void findFurthestPoint(vi xPts, vi yPts, db r)
   }
 }
 
-void purePursuit(db minRadius, vi xPts, vi yPts)
+void purePursuit(db minRadius, vi xPts, vi yPts){
+  
+}
