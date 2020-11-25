08b01263ede910216e352e40a4a27cff3ce9a142
arakich <j3shw4@gmail.com>
2020-11-25 15:07:19 -0500

    br

--- src/PurePursuit.cpp
+++ src/PurePursuit.cpp
@@ -18,11 +18,27 @@ vi circLineIntersect(db xl, db yl, db a, db b, db r, db xc, db yc){
 
   db ret1 = (negB + sqrtf(radical))/denom;
   db ret2 = (negB - sqrtf(radical))/denom;
-if(ret1 >= 0.0 && ret1 <= 1.0)
-    ret.push_back(ret1);
-if(ret2 >= 0.0 && ret2 <= 1.0)
-    ret.push_back(ret2);
+  if(ret1 >= 0.0 && ret1 <= 1.0)
+      ret.push_back(ret1);
+  if(ret2 >= 0.0 && ret2 <= 1.0)
+      ret.push_back(ret2);
   return ret;
+}
+
+void findFurthestPoint(vi xPts, vi yPts, db r)
+{
+  int maxIndex;
+  db a;
+  db b;
+  db x0;
+  db y0;
+  for(int i = 0; i < xPts.size()-1; i++) {
+    x0 = xPts[i];
+    y0 = yPts[i];
+    a = xPts[i+1] - x0;
+    b = yPts[i+1] - y0;
+
+  }
 }
 
 void purePursuit(db minRadius, vi xPts, vi yPts)
