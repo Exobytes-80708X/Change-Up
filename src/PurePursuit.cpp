1a1bf0659ac6669bf42287b4747ac01cc68ba9b9
RushilSidhu <49910626+RushilSidhu@users.noreply.github.com>
2020-11-25 14:59:41 -0500

    restricted pp

--- src/PurePursuit.cpp
+++ src/PurePursuit.cpp
@@ -16,7 +16,13 @@ vi circLineIntersect(db xl, db yl, db a, db b, db r, db xc, db yc){
   db negB = -(a*x0 + b*y0);
   db denom = a*a + b*b;
 
-  ret.push_back((negB + sqrtf(radical))/denom);
-  ret.push_back((negB - sqrtf(radical))/denom);
+  db ret1 = (negB + sqrtf(radical))/denom;
+  db ret2 = (negB - sqrtf(radical))/denom;
+if(ret1 >= 0.0 && ret1 <= 1.0)
+    ret.push_back(ret1);
+if(ret2 >= 0.0 && ret2 <= 1.0)
+    ret.push_back(ret2);
   return ret;
 }
+
+void purePursuit(db minRadius, vi xPts, vi yPts)
